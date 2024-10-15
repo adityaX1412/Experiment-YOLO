fa11fd3fbff114e6be38ee530c7b9bd077b7561b
adityaX1412 <148233635+adityaX1412@users.noreply.github.com>
2024-10-15 23:59:35 +0530

    Update dataset.py

--- ultralytics/data/dataset.py
+++ ultralytics/data/dataset.py
@@ -277,8 +277,9 @@ class ClassificationDataset(torchvision.datasets.ImageFolder):
     def __getitem__(self, i):
         """Returns subset of data and targets corresponding to given indices."""
         f, j, fn, im = self.samples[i]  # filename, index, filename.with_suffix('.npy'), image
-        if self.cache_ram and im is None:
-            im = self.samples[i][3] = cv2.imread(f)
+        if self.cache_ram:
+            if im is None:  # Warning: two separate if statements required here, do not combine this with previous line
+                im = self.samples[i][3] = cv2.imread(f)
         elif self.cache_disk:
             if not fn.exists():  # load npy
                 np.save(fn.as_posix(), cv2.imread(f), allow_pickle=False)
@@ -289,6 +290,7 @@ class ClassificationDataset(torchvision.datasets.ImageFolder):
         im = Image.fromarray(cv2.cvtColor(im, cv2.COLOR_BGR2RGB))
         sample = self.torch_transforms(im)
         return {"img": sample, "cls": j}
+
 
     def __len__(self) -> int:
         """Return the total number of samples in the dataset."""
