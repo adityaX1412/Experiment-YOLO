bf908a1695b198e391291dcfb794c89641d8e4e7
adityax1412 <adityanaidu2004@gmail.com>
2025-01-09 17:14:59 +0530

    moo

--- val.py
+++ val.py
@@ -1,9 +1,13 @@
 import warnings
 warnings.filterwarnings('ignore')
 from ultralytics import YOLO
+import torch
 
 if __name__ == '__main__':
     model = YOLO('/kaggle/input/nano-softshare-weights/yolov8_softshare_waid.pt')
+    checkpoint_path = '/kaggle/input/nano-softshare-weights/yolov8_softshare_waid.pt'
+    ckpt = torch.load(checkpoint_path, map_location='cpu')
+    print(ckpt.keys())
     model.val(data='/kaggle/input/waiddataset/WAID-main/WAID-main/WAID/data.yaml',
               split='test',
               imgsz=640,
@@ -13,3 +17,4 @@ if __name__ == '__main__':
               project='runs/val',
               name='yolov8m-ASF',
               )
+    
