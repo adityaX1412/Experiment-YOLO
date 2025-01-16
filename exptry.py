89553a74ef9f7a50e83b2a1cd30e87384a1b91c2
Hem Gosalia <149922474+HemGosalia@users.noreply.github.com>
2025-01-16 18:22:41 +0530

    Update exptry.py

--- exptry.py
+++ exptry.py
@@ -9,7 +9,7 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 # Initialize a new W&B run
 wandb.login(key="833b800ff23eb3d26e6c85a8b9e1fc8bbafc9775") 
-wandb.init(project="yolov8_Moo_moo")
+wandb.init(project="yolov8_buck")
 # Load the custom model configuration
 model = YOLO('yolov8n-ASF-P2.yaml')
 model.model.to(device)
@@ -33,7 +33,7 @@ model.add_callback('on_train_batch_end', log_losses)
 
 # Train the model with the specified configuration and sync to W&B
 Result_Final_model = model.train(
-    data="/kaggle/input/aerial-cows-yolov8/data.yaml",
+    data="/kaggle/input/bucktales/yolov8_format_v1/yolov8_format_v1/dtc2023.yaml",
     epochs=70,
     batch=8,
     optimizer='SOAP',
