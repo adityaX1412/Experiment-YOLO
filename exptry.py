03f852a0c5321856226be4e2ba61915f63ddb627
Hem Gosalia <149922474+HemGosalia@users.noreply.github.com>
2025-01-21 17:20:00 +0530

    yolov8 vanilla run on bucktales patched

--- exptry.py
+++ exptry.py
@@ -8,10 +8,9 @@ import os
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 # Initialize a new W&B run
-wandb.login(key="833b800ff23eb3d26e6c85a8b9e1fc8bbafc9775") 
 wandb.init(project="yolov8_buck_patched")
 # Load the custom model configuration
-model = YOLO('yolov8n-ASF-P2.yaml')
+model = YOLO('yolov8n.yaml')
 model.model.to(device)
 
 # Define a callback to log losses at the end of each training batch
@@ -41,7 +40,7 @@ Result_Final_model = model.train(
     save=True,
 )
 # Define model and dataset names
-model_name = "yolov8_softspd"
+model_name = "yolov8_vanilla"
 dataset_name = "bucktales-patched"
 
 # Save the model as .pth file in Kaggle workspace
