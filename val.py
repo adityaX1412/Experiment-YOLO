300d788119fb5ba17733152046b514b5a3a1f754
adityax1412 <adityanaidu2004@gmail.com>
2025-01-26 10:19:06 +0530

    .

--- val.py
+++ val.py
@@ -3,7 +3,8 @@ warnings.filterwarnings('ignore')
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    model = YOLO('/kaggle/input/yolo-weights/weights/spdn soap.pt')
+    model = YOLO('/kaggle/input/yolo-weights/weights/spdn  soap.pt')
+    # /kaggle/input/yolo-weights/weights/spdnsoap.pt
     model.val(data='/kaggle/input/waiddataset/WAID-main/WAID-main/WAID/data.yaml',
               split='test',
               imgsz=640,
