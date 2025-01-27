ff5930b1a572c88c6d4d569b35ca174c099f2492
adityax1412 <adityanaidu2004@gmail.com>
2025-01-27 18:18:20 +0530

    exp

--- val.py
+++ val.py
@@ -5,7 +5,7 @@ from ultralytics import YOLO
 if __name__ == '__main__':
     model = YOLO('/kaggle/input/waid-no-soap/spdnosoap.pt')
     # /kaggle/input/yolo-weights/weights/spdnsoap.pt
-    model.val(data='/kaggle/input/aerial-cows-yolov8/data.yaml',
+    model.val(data='/kaggle/input/waiddataset/WAID-main/WAID-main/WAID/data.yaml',
               split='test',
               imgsz=640,
               batch=16,
