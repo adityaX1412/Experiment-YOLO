6cd7f7ac929317d426563adcfa06a6bf5ab7607b
adityax1412 <adityanaidu2004@gmail.com>
2025-02-04 18:58:31 +0530

    meth

--- val.py
+++ val.py
@@ -5,7 +5,7 @@ from ultralytics import YOLO
 if __name__ == '__main__':
     model = YOLO('/kaggle/input/bucktales-vanilla-with-soap/vanilla soap.pt')
     # /kaggle/input/yolo-weights/weights/spdnsoap.pt
-    model.val(data='/kaggle/input/bucktales/dtc2023.yaml',
+    model.val(data='/kaggle/input/bucktales-patched/dtc2023.yaml',
               split='test',
               imgsz=1280,
               batch=16,
