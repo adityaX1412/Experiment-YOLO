f6f0a00c3203a84962ea6dc12d0a026b942427d2
adityax1412 <adityanaidu2004@gmail.com>
2025-02-05 17:49:17 +0530

    exp

--- val.py
+++ val.py
@@ -3,11 +3,11 @@ warnings.filterwarnings('ignore')
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    model = YOLO('/kaggle/input/bucktales-vanilla-with-soap/vanilla soap.pt')
+    model = YOLO('/kaggle/input/bucktale-weights/ssff soap.pt')
     # /kaggle/input/yolo-weights/weights/spdnsoap.pt
     model.val(data='/kaggle/input/bucktales-patched/dtc2023.yaml',
               split='test',
-              imgsz=1280,
+              imgsz=640,
               batch=16,
               # rect=False,
               # save_json=True, # if you need to cal coco metrice
