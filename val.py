a8d8a1632717ae6da86c6d112cd5454a70c63eba
adityax1412 <adityanaidu2004@gmail.com>
2025-02-07 01:10:33 +0530

    squabble up

--- val.py
+++ val.py
@@ -3,14 +3,14 @@ warnings.filterwarnings('ignore')
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    model = YOLO('/kaggle/input/bucktale-weights/ssfflossp2soapvarifocal.pt')
+    model = YOLO('/kaggle/input/yolo-weights/weights/spdld.pt')
     # /kaggle/input/yolo-weights/weights/spdnsoap.pt
-    model.val(data='/kaggle/input/bucktales-patched/dtc2023.yaml',
+    model.val(data='/kaggle/input/waiddataset/WAID-main/WAID-main/WAID/data.yaml',
               split='test',
               imgsz=640,
               batch=16,
               # rect=False,
-              # save_json=True, # if you need to cal coco metrice
+              save_json=True, # if you need to cal coco metrice
               project='runs/val',
               name='yolov8n-spdld',
               )
