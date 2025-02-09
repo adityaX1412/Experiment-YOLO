8f82b266d7edfb998a5b0887ca8b58ef74afc217
adityax1412 <adityanaidu2004@gmail.com>
2025-02-09 14:50:06 +0530

    yuh

--- detect.py
+++ detect.py
@@ -3,7 +3,7 @@ warnings.filterwarnings('ignore')
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    model = YOLO('/kaggle/input/ld70-waid-weight/best (3).pt') # select your model.pt path
+    model = YOLO('/kaggle/input/yolo-weights/weights/spdld.pt') # select your model.pt path
     model.predict(source='/kaggle/input/waiddataset/WAID-main/WAID-main/WAID/data.yaml',
                   imgsz=640,
                   project='runs/detect',
