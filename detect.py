20c718d3b01f59bc221eccac816bff3b8da412e1
adityax1412 <adityanaidu2004@gmail.com>
2025-02-09 14:51:29 +0530

    yuh

--- detect.py
+++ detect.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
     model = YOLO('/kaggle/input/yolo-weights/weights/spdld.pt') # select your model.pt path
-    model.predict(source='/kaggle/input/waiddataset/WAID-main/WAID-main/WAID/data.yaml',
+    model.predict(source="/kaggle/input/waiddataset/WAID-main/WAID-main/WAID/images/test",
                   imgsz=640,
                   project='runs/detect',
                   name='exp',
