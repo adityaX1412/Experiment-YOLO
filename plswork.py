440201381a23b14e32256af0ec72cd879a2dc22c
adityax1412 <adityanaidu2004@gmail.com>
2025-02-10 17:36:15 +0530

    non

--- plswork.py
+++ plswork.py
@@ -16,7 +16,7 @@ from PIL import ImageDraw
 IMAGE_DIR = "/kaggle/input/waiddataset/WAID-main/WAID-main/WAID/images/test"
 LABEL_DIR = "/kaggle/input/waiddataset/WAID-main/WAID-main/WAID/labels/test"
 DATA_YAML = "/kaggle/input/waiddataset/WAID-main/WAID-main/WAID/data.yaml"
-MODEL_WEIGHTS = "/kaggle/input/ld70-waid-weight/best (3).pt"
+MODEL_WEIGHTS = "/kaggle/input/yolo-weights/weights/spdnwuloss.pt"
 CONF_THRESHOLD = 0.25
 IOU_THRESHOLD = 0.5 
 NMS_IOU_THRESHOLD = 0.45
@@ -56,7 +56,7 @@ os.makedirs(FINAL_VIS_DIR, exist_ok=True)
 inference_times = []
 gflops_values = []
 
-predictions_path = "/kaggle/input/ld70-json/predictions (1).json"
+predictions_path = "/kaggle/input/json-files/spdnwuloss.json"
 if not os.path.exists(predictions_path):
     raise FileNotFoundError(f"❌ Predictions file not found at {predictions_path}")
 
