3852715bd86ea7bf72cd62690e192f319fde4bf7
adityax1412 <adityanaidu2004@gmail.com>
2025-02-10 17:17:22 +0530

    non

--- val.py
+++ val.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     # /kaggle/input/yolo-weights/weights/spdnsoap.pt
     model.val(data='/kaggle/input/bucktales-patched/bucktales_patched/dtc2023.yaml',
               split='test',
-              imgsz=640,
+              imgsz=1280,
               batch=16,
               # rect=False,
               save_json=False, # if you need to cal coco metrice
