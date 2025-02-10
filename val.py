30591b524ce984c369c075510de5932167dd3722
adityax1412 <adityanaidu2004@gmail.com>
2025-02-10 17:06:36 +0530

    non

--- val.py
+++ val.py
@@ -5,7 +5,7 @@ from ultralytics import YOLO
 if __name__ == '__main__':
     model = YOLO('/kaggle/input/bucktales-retdetr-weight/RETDETR.pt')
     # /kaggle/input/yolo-weights/weights/spdnsoap.pt
-    model.val(data='/kaggle/input/bucktales-patched/bucktales_patched/dtc2023_RTDETR_Colab.yaml',
+    model.val(data='/kaggle/input/bucktales-patched/bucktales_patched/dtc2023.yaml',
               split='test',
               imgsz=640,
               batch=16,
