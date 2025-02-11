a93496bdde46c6d46e1c9b944bd4f6534892ca93
Hem Gosalia <149922474+xxHeisenberg31xx@users.noreply.github.com>
2025-02-11 09:52:08 +0530

    benchmark

--- val.py
+++ val.py
@@ -2,15 +2,97 @@ import warnings
 warnings.filterwarnings('ignore')
 from ultralytics import YOLO
 
+
+# if __name__ == '__main__':
+#     model = YOLO('C:/Users/hemgo/Desktop/AI/best_yolov8n_bucktales_patched.pt')
+#     model.val(data='C:/Users/hemgo/Desktop/AI/archive/dtc2023_local_unpatched.yaml',
+#               split='test',
+#               imgsz=1280,
+#               batch=4,
+#               # rect=False,
+#               # save_json=True, # if you need to cal coco metrics
+#               project='runs/val1',
+#               name='yolov8n-vanilla-unpatched-1280',
+#               plots=True,
+#               )
+    
+#     print('\n2\n')
+
+
 if __name__ == '__main__':
-    model = YOLO('C:/Users/hemgo/Desktop/AI/best_yolov9t_bucktales_patched.pt')
-    model.val(data='C:/Users/hemgo/Desktop/AI/archive(1)_patched/dtc2023_local.yaml',
+    model = YOLO('C:/Users/hemgo/Desktop/AI/best_SSFF+loss+p2+soap_bucktales_patched.pt')
+    model.val(data='C:/Users/hemgo/Desktop/AI/archive/dtc2023_local_unpatched.yaml',
               split='test',
-              imgsz=640,
-              batch=16,
+              imgsz=1280,
+              batch=4,
               # rect=False,
               # save_json=True, # if you need to cal coco metrics
-              project='runs/val',
-              name='yolov9t-vanilla',
+              project='runs/val1',
+              name='SSFF+loss+p2+soap-unpatched-1280',
               plots=True,
               )
+    
+    print('\n3\n')
+
+# if __name__ == '__main__':
+#     model = YOLO('C:/Users/hemgo/Desktop/AI/best_yolov8n_bucktales_patched.pt')
+#     model.val(data='C:/Users/hemgo/Desktop/AI/archive/dtc2023_local_unpatched.yaml',
+#               split='test',
+#               imgsz=2560,
+#               batch=4,
+#               # rect=False,
+#               # save_json=True, # if you need to cal coco metrics
+#               project='runs/val1',
+#               name='yolov8n-vanilla-unpatched-2560',
+#               plots=True,
+#               )
+    
+#     print('\n4\n')
+
+
+if __name__ == '__main__':
+    model = YOLO('C:/Users/hemgo/Desktop/AI/best_SSFF+loss+p2+soap_bucktales_patched.pt')
+    model.val(data='C:/Users/hemgo/Desktop/AI/archive/dtc2023_local_unpatched.yaml',
+              split='test',
+              imgsz=2560,
+              batch=4,
+              # rect=False,
+              # save_json=True, # if you need to cal coco metrics
+              project='runs/val1',
+              name='SSFF+loss+p2+soap-unpatched-2560',
+              plots=True,
+              )
+    
+    print('\n5\n')
+    
+    
+# if __name__ == '__main__':
+#     model = YOLO('C:/Users/hemgo/Desktop/AI/best_yolov8n_bucktales_patched.pt')
+#     model.val(data='C:/Users/hemgo/Desktop/AI/archive/dtc2023_local_unpatched.yaml',
+#               split='test',
+#               imgsz=3840,
+#               batch=4,
+#               # rect=False,
+#               # save_json=True, # if you need to cal coco metrics
+#               project='runs/val1',
+#               name='yolov8n-vanilla-unpatched-3840',
+#               plots=True,
+#               )
+    
+#     print('\n6\n')
+
+
+if __name__ == '__main__':
+    model = YOLO('C:/Users/hemgo/Desktop/AI/best_SSFF+loss+p2+soap_bucktales_patched.pt')
+    model.val(data='C:/Users/hemgo/Desktop/AI/archive/dtc2023_local_unpatched.yaml',
+              split='test',
+              imgsz=3840,
+              batch=4,
+              # rect=False,
+              # save_json=True, # if you need to cal coco metrics
+              project='runs/val1',
+              name='SSFF+loss+p2+soap-unpatched-3840',
+              plots=True,
+              )
+    
+    print('\n7\n')
