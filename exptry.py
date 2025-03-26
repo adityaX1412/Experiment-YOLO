1431e0cd7bf9adf216d3578563fc2818a32a1283
adityax1412 <adityanaidu2004@gmail.com>
2025-03-26 18:41:53 +0530

    rtdetr

--- exptry.py
+++ exptry.py
@@ -1,33 +1,29 @@
-from ultralytics.models.yolo import YOLO
-from ultralytics import YOLO
-from ultralytics.engine.model import Model
+from ultralytics import RTDETR
 import torch
 import wandb
 import os
+
 # Check if CUDA is available and set the device
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 # Initialize a new W&B run
-wandb.init(project="yolo_mew_exp")
-# Load the custom model configuration
-model = YOLO('yolov8n-GOLDED copy.yaml')
+wandb.init(project="rtdetr_exp")
+
+# Load the RT-DETR model configuration
+model = RTDETR('rtdetr.yaml')
 model.model.to(device)
 
 # Define a callback to log losses at the end of each training batch
 def log_losses(trainer):
-    # Access the loss dictionary
     loss_items = trainer.loss_items
-    
-    # Log each loss component
     wandb.log({
         "train/box_loss": loss_items[0],
         "train/cls_loss": loss_items[1],
         "train/dfl_loss": loss_items[2]
     }, step=trainer.epoch)
-
     torch.cuda.empty_cache()
 
-# Register the callback with the YOLO model
+# Register the callback with the RT-DETR model
 model.add_callback('on_train_batch_end', log_losses)
 
 # Train the model with the specified configuration and sync to W&B
@@ -36,9 +32,11 @@ Result_Final_model = model.train(
     epochs=70,
     batch=4,
     optimizer='SOAP',
-    project='yolo_new_exp',
+    project='rtdetr_new_exp',
     save=True,
-    imgsz = 640
+    imgsz=640
 )
+
 # Finish the W&B run
 wandb.finish()
+
